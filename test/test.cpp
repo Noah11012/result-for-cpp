39971516b4f215dc8cfb46ce152e663d35fb0965
Noah Nichols <1101qbtext@gmail.com>
2018-09-14 14:50:17 -0400

    Updated test to be more modular

--- test/test.cpp
+++ test/test.cpp
@@ -2,23 +2,24 @@
 #include <iostream>
 #include <string>
 
+int test1()
+{
+    rust::Result<int, std::string> r(100);
+
+    if(!r.is_ok())
+        return -1;
+    
+    rust::Result<int, std::string> r2 = rust::Result<int, std::string>::from_error("An error");
+
+    if(r.is_err())
+        return -1;
+}
 
 int main()
 {
-    rust::Result<int, std::string> result_test(100);
+    int status = 0;
 
-    if(!result_test.is_ok())
-    {
-        return 1;
-    }
+    status = test1();
 
-    rust::Result<int, std::string> result_test2 = rust::Result<int, std::string>::from_error("An error has occurred");
-    
-
-    if(!result_test2.is_err())
-    {
-        return 1;
-    }
-
-    return 0;
+    return status;
 }
